465a83d905ac6ec2ec4476ef4d8fcc7de0e08a81
Rafael Bordin <rafael.bordin@live.com>
2020-10-08 11:58:28 -0300

    Added signal treatment

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const process = require('process'); 
 const app = express();
 const auth = require("./src/middlewares/is_auth.middleware");
+
+process.on('SIGINT', function onSigint() {
+  app.shutdown();
+});
+
+process.on('SIGTERM', function onSigterm() {
+  app.shutdown();
+});
+
+app.shutdown = function () {
+  process.exit();
+};
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(auth);
+
 
 app.use(require("./routes"));
 
