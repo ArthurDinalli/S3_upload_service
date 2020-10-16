30f3dd2fd2bc1fdf83903dfe58388a0695d8f0f7
Rafael Bordin <rafael.bordin@live.com>
2020-10-16 18:11:54 -0300

    Updated authentication middleware for production

--- src/middlewares/is_auth.middleware.js
+++ src/middlewares/is_auth.middleware.js
@@ -5,48 +5,26 @@ module.exports = async (req, res, next) => {
 
 	if (!token) return res.status(401).send();
 
-	req.auth_user = {
-		name: "Marcelo Ivo",
-		cpf: "11231231231",
-		cellphone: "1231231231231",
-		email: "email@email.com",
-		id: 1,
-		activated: 1,
-		groups: [
-			{
-				code: "code",
-				title: "Grupo top",
-				is_department_master: 1,
+	const url = process.env.AUTH_URL;
+	request.get(
+		url,
+		{
+			headers: {
+				Bearer: token,
 			},
-		],
-	};
-	next();
+		},
+		(err, response, body) => {
+			if (err) {
+				console.log(err);
+				return res.status(401).send();
+			}
 
-	// const url = `http://auth-api:3000/internal/auth`;
-
-	// request.get(
-	// 	url,
-	// 	{
-	// 		headers: {
-	// 			Bearer: token,
-	// 		},
-	// 	},
-	// 	(err, response, body) => {
-	// 		if (err) {
-	// 			console.log(err);
-	// 			return res.status(401).send();
-	// 		}
-
-	// 		if (response.statusCode !== 200)
-	// 			return res.status(401).send();
-
-	// 		body = JSON.parse(body);
-
-	// 		req.auth_user = body;
-
-	// 		console.log(req.auth_user);
-
-	// 		next();
-	// 	}
-	// );
+			if (response.statusCode !== 200)
+				return res.status(401).send();
+			
+			body = JSON.parse(body);
+			req.auth_user = body;
+			next();
+		}
+	);
 };
